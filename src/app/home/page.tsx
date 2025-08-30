db2d265b8193e86803ef139b351bfa5c420eb50a
Luiz-Andalezio <luizandalecio19@gmail.com>
2025-08-30 19:14:46 -0300

    test(home): Adds a test home page.

---
+++ src/app/home/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import Link from "next/link"; 
+
+function Home() {
+    return (
+        <div className="min-h-screen flex flex-col justify-center items-center bg-readowl-purple-extralight"></div>
+    );
+}
+
+export default Home;
