83a601d1638cdcfd101b2efb7757d60ea461a2b1
Luiz-Andalezio <luizandalecio19@gmail.com>
2025-09-06 01:55:54 -0300

    feat(image-hosts): Adds images supporters hosts into next config.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,30 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'illusia.com.br' }, // Your own domain or project host
+      { protocol: 'https', hostname: 'res.cloudinary.com' }, // Cloudinary image hosting
+      { protocol: 'https', hostname: 'lh3.googleusercontent.com' }, // Google user content (Google Photos, etc.)
+      { protocol: 'https', hostname: 'i.imgur.com' }, // Imgur direct image links
+      { protocol: 'https', hostname: 'imgur.com' }, // Imgur main domain
+      { protocol: 'https', hostname: 'images.unsplash.com' }, // Unsplash image CDN
+      { protocol: 'https', hostname: 'pbs.twimg.com' }, // Twitter image CDN
+      { protocol: 'https', hostname: 'ibb.co' }, // imgbb image hosting (short links)
+      { protocol: 'https', hostname: 'i.ibb.co' }, // imgbb direct image links
+      { protocol: 'https', hostname: 'cdn.discordapp.com' }, // Discord CDN for attachments
+      { protocol: 'https', hostname: 'media.discordapp.net' }, // Discord CDN for media previews
+      { protocol: 'https', hostname: 'imageshack.com' }, // Imageshack main domain
+      { protocol: 'https', hostname: 'i.imageshack.com' }, // Imageshack direct image links
+      { protocol: 'https', hostname: 'postimg.cc' }, // PostImage image hosting
+      { protocol: 'https', hostname: 'i.postimg.cc' }, // PostImage direct image links
+      { protocol: 'https', hostname: 'flickr.com' }, // Flickr main domain
+      { protocol: 'https', hostname: 'live.staticflickr.com' }, // Flickr image CDN
+      { protocol: 'https', hostname: 'tinypic.com' }, // TinyPic image hosting (now defunct)
+      { protocol: 'https', hostname: 'i.pinimg.com' }, // Pinterest image CDN
+      { protocol: 'https', hostname: 'static.wikia.nocookie.net' } // Fandom/Wikia static assets
+    ],
+  },
 };
 
 export default nextConfig;
